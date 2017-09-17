25d17380a8edb7622d8cf80189d51ab90a72ceb1
Vinícius Feitosa da Silva <viniciusfesil@gmail.com>
2017-09-17 03:13:23 -0300

    [UPDATE] Adding click action calling onSelect Function. [UPDATE] Creating onSelect function on AppComponent class

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -24,15 +24,15 @@ const HEROES: Hero[] = [
   template: `<h1>{{title}}</h1>
             <h2>My Heroes</h2>
             <ul class="heroes">
-              <li *ngFor="let hero of heroes">
+              <li *ngFor="let hero of heroes" (click)="onSelect(hero)">
                 <span class="badge">{{hero.id}}</span> {{hero.name}}
               </li>
             </ul>
-            <h2>{{hero.name}} details!</h2>
-            <div><label>id: </label>{{hero.id}}</div>
+            <h2>{{selectedHero.name}} details!</h2>
+            <div><label>id: </label>{{selectedHero.id}}</div>
             <div>
-              <label>name: </label>{{hero.name}}
-              <input [(ngModel)]="hero.name" placeholder="name">
+              <label>name: </label>{{selectedHero.name}}
+              <input [(ngModel)]="selectedHero.name" placeholder="name">
             </div>
              `,
   // styleUrls: ['./app.component.css']
@@ -90,5 +90,10 @@ const HEROES: Hero[] = [
 export class AppComponent {
   title = 'Tour of Heroes';
   heroes = HEROES;
+  selectedHero: Hero;
+  
 
+  onSelect(hero: Hero) :void {
+    this.selectedHero = hero;
+  }
 }
