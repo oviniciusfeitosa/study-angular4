17716bfa80481a40bfe12a3c5fdff486534b814c
Vinícius Feitosa da Silva <viniciusfesil@gmail.com>
2017-09-17 02:03:26 -0300

    [UPDATE] Adding two way binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,7 +11,10 @@ export class Hero {
   template: `<h1>{{title}}</h1>
              <h2>{{hero.name}} details!</h2>
              <div><label>id: </label>{{hero.id}}</div>
-             <div><label>name: </label>{{hero.name}}</div>
+             <div>
+              <label>name: </label>{{hero.name}}
+              <input [(ngModel)]="hero.name" placeholder="name">
+             </div>
              `,
   styleUrls: ['./app.component.css']
 })
