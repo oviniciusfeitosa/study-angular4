861891185e1f896c4a23b8eca877b5546f3f7065
Vinícius Feitosa da Silva <viniciusfesil@gmail.com>
2017-09-17 01:55:27 -0300

    [UPDATE] Using inline template string anda hero object of type Hero initializated by object with id and name properties.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
 
+export class Hero {
+  id: number;
+  name: string;
+}
+
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  // templateUrl: './app.component.html',
+  template: '<h1>{{title}}</h1><h2>{{hero.name}} details!</h2>',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Angular4 App';
+  title = 'Tour of Heroes';
+  hero : Hero = {
+    id: 1,
+    name: 'Windstorm'
+  };
 }
