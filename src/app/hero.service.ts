f1d011a28a3e3e16f17cdf43cecd08d2d5c01169
Vinícius Feitosa da Silva <viniciusfesil@gmail.com>
2017-09-24 20:46:24 -0300

    [ADD] Adding getHeroes return type and returning Heroes constant with mocked info.

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,10 +1,13 @@
 import { Injectable } from '@angular/core';
 
-
+import { HEROES } from './mock-heroes';
+import { Hero } from './hero';
 
 //@Injectable Decorator
 @Injectable()
 
 export class HeroService {
-    getHeroes(): void {}
+    getHeroes(): Hero[] {
+        return Heroes
+    }
 }
