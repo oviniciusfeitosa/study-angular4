f161fead6b7eaaf631ccbe2ba8ae385b7ae787ea
Vinícius Feitosa da Silva <viniciusfesil@gmail.com>
2017-10-01 23:34:55 -0300

    [ADD/UPDATE] Creating and setting dashboard component.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,8 +4,9 @@ import { FormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
-import { HeroDetailComponent } from './hero-detail.component';
 import { HeroesComponent } from './heroes.component';
+import { DashboardComponent } from './dashboard.component';
+import { HeroDetailComponent } from './hero-detail.component';
 import { HeroService } from './hero.service';
 
 @NgModule({
@@ -17,6 +18,10 @@ import { HeroService } from './hero.service';
                 path: 'heroes',
                 // When call 'heroes' at base url '/' defined on index.html, the HeroesComponent will be called.
                 component: HeroesComponent
+            },
+            {
+                path: 'dashboard',
+                component: DashboardComponent
             }
         ])
     ],
@@ -24,7 +29,8 @@ import { HeroService } from './hero.service';
         AppComponent,
         HeroDetailComponent,
         // Declarations of HeroesComponent to make Angular recognize `<my-heroes>` tags.
-        HeroesComponent
+        HeroesComponent,
+        DashboardComponent
     ],
     providers: [
         // Teaching Injector do make instance of HeroService
