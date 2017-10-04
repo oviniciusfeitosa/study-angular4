ef97e7b6be06317e28199e4a93591b217a616961
Vinícius Feitosa da Silva <viniciusfesil@gmail.com>
2017-10-04 01:19:34 -0300

    [UPDATE] importing ActivatedRoute and ParamMap class to provide route parametes and HeroService to get specific data from Hero by param.

--- src/app/hero-detail.component.ts
+++ src/app/hero-detail.component.ts
@@ -1,5 +1,11 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { ActivatedRoute, ParamMap } from '@angular/router';
+import { Location } from '@angular/common';
+
+import { HeroService } from './hero.service';
 import { Hero } from './hero';
+
+import 'rxjs/add/operator/switchMap';
 
 // @Component Decorator
 @Component({
@@ -19,6 +25,19 @@ import { Hero } from './hero';
 })
 
 // export is used becouse every time it will be imported.
-export class HeroDetailComponent {
+export class HeroDetailComponent implements OnInit {
+
     @Input() hero: Hero;
+
+    constructor(
+        private heroService: HeroService,
+        private route: ActivatedRoute,
+        private location: Location
+    ) {}
+
+    ngOnInit(): void {
+        this.route.paramMap
+            .switchMap((params: ParamMap) => this.heroService.getHero(+params.get('id')))
+            .subscribe(hero => this.hero = hero);
+    }
 }
