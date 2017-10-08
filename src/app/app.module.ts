463e61ba964e20ca2e1a9749b7a78e14a7cef061
Vinícius Feitosa da Silva <viniciusfesil@gmail.com>
2017-10-08 17:47:10 -0300

    [UPDATE] Replacing old calls for routes to import of app-routing.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,45 +1,27 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
-import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
-import { HeroesComponent } from './heroes.component';
 import { DashboardComponent } from './dashboard.component';
 import { HeroDetailComponent } from './hero-detail.component';
+import { HeroesComponent } from './heroes.component';
 import { HeroService } from './hero.service';
+
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
     imports: [
         BrowserModule,
         FormsModule,
-        RouterModule.forRoot([
-            {
-                path: '',
-                redirectTo: '/dashboard',
-                pathMatch: 'full'
-            },
-            {
-                path: 'heroes',
-                // When call 'heroes' at base url '/' defined on index.html, the HeroesComponent will be called.
-                component: HeroesComponent
-            },
-            {
-                path: 'dashboard',
-                component: DashboardComponent
-            },
-            {
-                path: 'detail/:id',
-                component: HeroDetailComponent
-            }
-        ])
+        AppRoutingModule
     ],
     declarations: [
         AppComponent,
+        DashboardComponent,
         HeroDetailComponent,
         // Declarations of HeroesComponent to make Angular recognize `<my-heroes>` tags.
         HeroesComponent,
-        DashboardComponent
     ],
     providers: [
         // Teaching Injector do make instance of HeroService
