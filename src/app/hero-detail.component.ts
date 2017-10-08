532669af6dbdb8c7095be1cabbe75ae2d7884c85
Vinícius Feitosa da Silva <viniciusfesil@gmail.com>
2017-10-08 16:57:29 -0300

    [ADD] Migrating content from 'hero-detail.component.ts' to 'hero-detail.component.html' file

--- src/app/hero-detail.component.ts
+++ src/app/hero-detail.component.ts
@@ -11,17 +11,7 @@ import 'rxjs/add/operator/switchMap';
 @Component({
     selector: 'hero-detail',
     template: `
-                <div *ngIf="hero">
-                    <h2>{{hero.name}} details!</h2>
-                    <div>
-                        <label>id: </label>{{hero.id}}
-                    </div>
-                    <div>
-                        <label>name: </label>
-                        <input [(ngModel)]="hero.name" placeholder="name" />
-                    </div>
-                    <button (click)="goBack()">Back</button>
-                </div>
+                
               `
 })
 
