00eb44080927b295a1003032a4b2ef50703ad2c3
Vinícius Feitosa da Silva <viniciusfesil@gmail.com>
2017-10-08 18:02:02 -0300

    [UPDATE] implementing the method 'gotoDetail' and injecting router of type Router in constructor of class.

--- src/app/heroes.component.ts
+++ src/app/heroes.component.ts
@@ -1,31 +1,36 @@
-import { Component } from '@angular/core';
-import { Hero } from './hero';
-import { HeroService } from './hero.service'
-import { OnInit } from '@angular/core';
+import {Component} from '@angular/core';
+import {Hero} from './hero';
+import {HeroService} from './hero.service'
+import {OnInit} from '@angular/core';
+import {Router} from "@angular/router";
 
 @Component({
-  selector: 'my-heroes',
-  templateUrl: './heroes.component.html',
-  styleUrls: ['./heroes.component.css']
+    selector: 'my-heroes',
+    templateUrl: './heroes.component.html',
+    styleUrls: ['./heroes.component.css']
 })
 
 export class HeroesComponent implements OnInit {
-  heroes: Hero[];
-  selectedHero: Hero;
+    heroes: Hero[];
+    selectedHero: Hero;
 
-  constructor(private heroService: HeroService) {
-    this.getHeroes()
-  }
+    constructor(private router: Router,
+                private heroService: HeroService) {
+    }
 
-  ngOnInit(): void {
-    this.getHeroes();
-  }
+    ngOnInit(): void {
+        this.getHeroes();
+    }
 
-  getHeroes(): void {
-    this.heroService.getHeroes().then(heroes => this.heroes = heroes);
-  }
+    getHeroes(): void {
+        this.heroService.getHeroes().then(heroes => this.heroes = heroes);
+    }
 
-  onSelect(hero: Hero): void {
-    this.selectedHero = hero;
-  }
+    onSelect(hero: Hero): void {
+        this.selectedHero = hero;
+    }
+
+    gotoDetail(): void {
+        this.router.navigate(['/detail', this.selectedHero.id]);
+    }
 }
